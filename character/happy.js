c03b2c8e71e15bb0524a657ae29e487ba079057f
ZhuYing-official <2306715965@qq.com>
2023-03-15 16:58:07 +0800

    修改孙悟空、妲己评级

--- character/happy.js
+++ character/happy.js
@@ -1712,11 +1712,11 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 			// g绿 b蓝 r红 p粉
 			cuishi:'#b捞得一评级:3.6',
 			liucong:'捞得一评级:1.0',
-			hok_daji:'#b捞得一评级:3.7',
+			hok_daji:'#b捞得一评级:3.8',
 			hok_lixin:'#r捞得一评级:4.2',
 			hok_makeboluo:'#b捞得一评级:3.9',
 			hok_mingshiyin:'#r耀世圣手评级:4.1',
-			hok_sunwukong:'#b捞得一评级:3.8',
+			hok_sunwukong:'#r捞得一评级:4.1',
 			shen_caozhi:'#r捞得一评级:4.2',
 			shen_dongzhuo:'#r捞得一评级:4.2',
 			shen_lusu:'#r捞得一评级:4.2',
