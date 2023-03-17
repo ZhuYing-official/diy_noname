ea57b8627828737708722c719123d3b52e2004d4
ZhuYing-official <2306715965@qq.com>
2023-03-18 00:43:02 +0800

    李信人杰路线[再起]调整为[罪论]

--- character/happy.js
+++ character/happy.js
@@ -17,8 +17,8 @@ function removeRenjie(player){
 	if(player.hasSkill('olqingyi')){
 		player.removeSkill('olqingyi');
 	}
-	if(player.hasSkill('rexuanhuo')){
-		player.removeSkill('rexuanhuo');
+	if(player.hasSkill('xinfu_zuilun')){
+		player.removeSkill('xinfu_zuilun');
 	}
 };
 function removeTongyu(player){
@@ -480,7 +480,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 				juexingji:true,
 				skillAnimation:true,
 				animationColor:'water',
-				derivation: ['hok_tongkuang','pozhu','olqingyi','rexuanhuo','reshuishi','lingce','dinghan','shencai','drlt_jieying','drlt_poxi'],
+				derivation: ['hok_tongkuang','pozhu','olqingyi','xinfu_zuilun','reshuishi','lingce','dinghan','shencai','drlt_jieying','drlt_poxi'],
 				filter:function(event,player){
 					return player.countMark('hok_wangming')>=3;
 				},
@@ -489,7 +489,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 					player.syncStorage('hok_wangming');
 					player.awakenSkill(event.name);
 					player.addSkill('hok_tongkuang');
-					player.addSkill('rexuanhuo');
+					player.addSkill('xinfu_zuilun');
 				},
 			},
 			hok_tongkuang:{
@@ -498,7 +498,7 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 				trigger: {player:'phaseJudgeBefore'},
 				forced:true,
 				filter:function(event,player){
-					return player.countMark('hok_wangming')>=2;
+					return player.countMark('hok_wangming')>=0;
 				},
 				// enable:'phaseUse',
 				usable:1,
@@ -527,18 +527,21 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 						// trigger:{player:'phaseUse'},
 						// frequent:true,
 						forced:true,
+						filter:function(event,player){
+							return player.countMark('hok_wangming')>=2;
+						},
 						// enable:'phaseUse',
 						usable:1,
 						content:function(){
-							event.lx = ['olqingyi','pozhu','rexuanhuo'];
+							event.lx = ['olqingyi','pozhu','xinfu_zuilun'];
 							if(player.hasSkill('pozhu')){
 								event.lx.splice(event.lx.indexOf('pozhu'),1)
 							}
 							if(player.hasSkill('olqingyi')){
 								event.lx.splice(event.lx.indexOf('olqingyi'),1)
 							}
-							if(player.hasSkill('rexuanhuo')){
-								event.lx.splice(event.lx.indexOf('rexuanhuo'),1)
+							if(player.hasSkill('xinfu_zuilun')){
+								event.lx.splice(event.lx.indexOf('xinfu_zuilun'),1)
 							}
 							'step 0'
 							player.chooseControl(event.lx).set('prompt','选择获得一个技能');
@@ -554,6 +557,9 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 						// trigger:{player:'phaseUse'},
 						// frequent:true,
 						forced:true,
+						filter:function(event,player){
+							return player.countMark('hok_wangming')>=2;
+						},
 						// enable:'phaseUse',
 						usable:1,
 						content:function(){
@@ -581,6 +587,9 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 						// trigger:{player:'phaseUse'},
 						// frequent:true,
 						forced:true,
+						filter:function(event,player){
+							return player.countMark('hok_wangming')>=2;
+						},
 						// enable:'phaseUse',
 						usable:1,
 						content:function(){
@@ -1847,9 +1856,9 @@ game.import('character',function(lib,game,ui,get,ai,_status){
 			hok_wangming:'王命',
 			hok_wangming_info:'锁定技，游戏开始时，你获得2枚「王」标记，你视为拥有当前主公的主公技；锁定技，当你造成/受到伤害后，你获得一枚「王」标记。',
 			hok_dengshen:'登神',
-			hok_dengshen_info:'觉醒技，准备阶段，若你武将牌上的「王」数不小于3，则你弃置2枚「王」，获得技能[统狂]、[眩惑]。',
+			hok_dengshen_info:'觉醒技，准备阶段，若你武将牌上的「王」数不小于3，则你弃置2枚「王」，获得技能[统狂]、[罪论]。',
 			hok_tongkuang:'统狂',
-			hok_tongkuang_info:'判断阶段，你选择[人杰]、[统御]、[狂暴]路线中的一个，失去其他路线的技能；弃牌阶段开始时，你可以弃置2枚「王」标记，获得该路线的一个技能。（人杰：[破竹][清议][眩惑]；统御：[慧识][灵策][定汉]；狂暴：[神裁][劫营][魄袭]。）',
+			hok_tongkuang_info:'判定阶段，你选择[人杰]、[统御]、[狂暴]路线中的一个，失去其他路线的技能；弃牌阶段开始时，你可以弃置2枚「王」标记，获得该路线的一个技能。（人杰：[破竹][清议][罪论]；统御：[慧识][灵策][定汉]；狂暴：[神裁][劫营][魄袭]。）',
 			hok_tongkuang_renjie:'人杰',
 			hok_tongkuang_tongyu:'统御',
 			hok_tongkuang_kuangbao:'狂暴',
