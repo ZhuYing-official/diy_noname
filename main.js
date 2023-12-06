d307851750af36cf62f65c4254ab737f31969f3f
ZhuYing-official <2306715965@qq.com>
2023-12-06 13:23:01 +0800

    更新客户端

--- main.js
+++ main.js
@@ -1,49 +1,36 @@
-const electron = require('electron');
-const {app} = electron;
-const {BrowserWindow} = electron;
-const fs = require('fs');
-const path = require('path');
+'use strict';
+
+const {
+	app,
+	BrowserWindow
+} = require('electron');
 
 let win;
 
-// 更改数据文件存放路径（from 诗笺）
-function createDir(dirPath) {
-	if (!fs.existsSync(dirPath)) {
-		fs.mkdirSync(dirPath);
-	}
-}
-
-function setPath(path1, path2) {
-	app.getPath(path1);
-	createDir(path2);
-	app.setPath(path1, path2);
-}
-
-// 将无名杀的数据放到客户端安装包目录中，即游戏目录/UserData内，做到删除后无残留，但会加长写入游戏设置的时间
-setPath('userData', path.join(__dirname, 'UserData'));
-
-// 获取单实例锁（from 诗笺）
-// const gotTheLock = app.requestSingleInstanceLock();
-// if (!gotTheLock) {
-// 	// 如果获取失败，说明已经有实例在运行了，直接退出
-// 	app.quit();
-// }
-
 function createWindow() {
-    win = new BrowserWindow({width: 960, height: 660, title:'无名杀'});
-    win.loadURL(`file://${__dirname}/app.html`);
-    win.on('closed', () => {
-        win = null;
-    });
+	win = new BrowserWindow({
+		width: 960,
+		height: 660,
+		title: '无名杀',
+		icon: require('path').resolve(__dirname, 'noname.ico'),
+		autoHideMenuBar: true,
+		webPreferences: {
+			nodeIntegration: true,
+			nodeIntegrationInSubFrames: true,
+			nodeIntegrationInWorker: true,
+			contextIsolation: false,
+			plugins: true,
+			experimentalFeatures: true
+		}
+	});
+	win.maximize();
+	win.loadURL(`file://${__dirname}/index.html`);
+	win.on('closed', () => win = null);
 }
 
 app.on('ready', createWindow);
-app.on('window-all-closed', () => {
-    app.quit();
-});
+app.on('window-all-closed', () => app.quit());
 
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
+	if (win === null) createWindow();
 });
