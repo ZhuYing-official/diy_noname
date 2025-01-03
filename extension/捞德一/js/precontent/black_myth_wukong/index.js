a06011eee9c48912247be001127446942d8c35e6
ZhuYing-official <2306715965@qq.com>
2025-01-04 00:58:18 +0800

    - 黑猴BGM

--- extension/捞德一/js/precontent/black_myth_wukong/index.js
+++ extension/捞德一/js/precontent/black_myth_wukong/index.js
@@ -14,6 +14,52 @@ import voices from './voices.js';
 import { characterSort, characterSortTranslate } from './sort.js';
 
 const packs = function () {
+	lib.skill._playAudio = {
+		charlotte: true,
+		ruleSkill: true,
+		forceDie: true,
+		trigger: { player: 'phaseBegin' },
+		filter(event, player) {
+			return player.name.startsWith('wukong_');
+		},
+		direct: true,
+		firstDo: true,
+		priority: Infinity,
+		content() {
+			if (lib.config.background_audio) {
+				'step 0'
+				game.saveConfig('extension_捞德一_bgm', lib.config.background_music);
+				game.saveConfig('background_music', 'ext:捞德一/audio/effect/wukong_bgm01.mp3');
+				'step 1'
+				game.playBackgroundMusic();
+			}
+		},
+	};
+	lib.skill._playDefaultAudio = {
+		charlotte: true,
+		ruleSkill: true,
+		forceDie: true,
+		trigger: { player: 'phaseBegin' },
+		filter(event, player) {
+			return !player.name.startsWith('wukong_') && lib.config.background_music == 'ext:捞德一/audio/effect/wukong_bgm01.mp3';
+		},
+		direct: true,
+		firstDo: true,
+		priority: Infinity,
+		content() {
+			try {
+				if (lib.config.extension_捞德一_bgm) {
+					game.saveConfig('background_music', lib.config.extension_捞德一_bgm);
+				} else {
+					game.saveConfig('background_music', 'music_default');
+				}
+				game.playBackgroundMusic();
+			} catch (e) {
+				alert(e.message)
+			}
+		},
+	};
+
 	const black_myth_wukong = {
 		name: 'black_myth_wukong',
 		connect: true,
@@ -45,7 +91,7 @@ const packs = function () {
 		nature: 'qunmm',
 		showName: '黑',
 	})
-	lib.translate['black_myth_wukong_character_config'] = '<span style="font-family: xingkai;font-size: 20px;">黑神话: 悟空</span>';
+	lib.translate['black_myth_wukong_character_config'] = `<span style='font-family: xingkai;font-size: 20px;'>黑神话: 悟空</span>`;
 	return black_myth_wukong;
 }
 export default packs;
